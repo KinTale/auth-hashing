56cd3b6750a3950faca7e3797a2cff496bb53f78
Kiran Gurung <kg820532@gmail.com>
2022-04-12 12:02:11 +0100

    complete

--- src/router.js
+++ src/router.js
@@ -6,12 +6,46 @@ const prisma = new PrismaClient();
 
 const router = express.Router();
 
+const secret = 'absolutesecretKey'
 router.post('/register', async (req, res) => {
-    
+
+    const password = req.body.password
+    const saltRounds = 10
+    const hash = bcrypt.hashSync(password, saltRounds)
+    console.log(hash)
+
+
+    const createUser = await prisma.user.create({
+        data: {
+            username: req.body.username,
+            password: hash
+        }
+    })
+    res.json({ user: createUser })
 });
 
+
 router.post('/login', async (req, res) => {
-    
+
+    const findUser = await prisma.user.findFirst({
+        where: {
+            username: req.body.username
+        }
+    })
+    //console.log(findUser)
+    if (!findUser) {
+        res.status(404)
+        res.json('user not found')
+    }
+    const verify = bcrypt.compareSync(req.body.password, findUser.password)
+    if (!verify) {
+        res.status(401)
+        res.json('incorrect password')
+    }
+
+    const token = jwt.sign(req.body.username, secret)
+   
+    res.json({ JWT: token })
 });
 
 module.exports = router;
